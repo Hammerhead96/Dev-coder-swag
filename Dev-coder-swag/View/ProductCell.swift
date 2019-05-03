056a42fb01cabf0bb5c03591b227ec2af5b938e7
Anthony Crane <anthonycrane@hotmail.com>
2019-05-02 19:43:55 -0700

    Complete
    The App is complete. It pulls its data from the App instead of a website, but could easily be adapted to pull from another source (so says Mark).

---
+++ Dev-coder-swag/View/ProductCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProductCell.swift
+//  Dev-coder-swag
+//
+//  Created by Mac User on 5/2/19.
+//  Copyright © 2019 Hammerhead96. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    func updateViews(product: Product) {
+        productImage.image = UIImage(named: product.imageName)
+        productTitle.text = product.title
+        productPrice.text = product.price
+    }
+}
